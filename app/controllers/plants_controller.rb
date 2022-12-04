1609fe4bff8891ae0a88a72f84882c6e16b1aebb
Kane Mathis <admin@kanemathis.com>
2022-12-04 16:49:37 -0500

    commit tree 14b479572b57e20c4622bb82ef73b4b8461d39a4

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -17,6 +17,23 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+# PATCH /plants
+  def update
+    plant=Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    end
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant=Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    end
+  end
 
   private
 
